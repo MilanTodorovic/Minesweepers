b589b9bac50ba988d21bdfea75f6af0a8e275b34
Milan Todorovic <todor-90@hotmail.com>
2018-04-22 21:16:00 +0200

    Now it works

--- minesweepers.py
+++ minesweepers.py
@@ -1,7 +1,20 @@
 import random
 GRID_SIZE = 20
-MINES = 20
+MINES = GRID_SIZE
 ALIVE = True
+RED   = "\033[1;31m"
+BLUE  = "\033[1;34m"
+CYAN  = "\033[1;36m"
+GREEN = "\033[0;32m"
+RESET = "\033[0;0m"
+BOLD  = "\033[;1m"
+REVERSE = "\033[;7m"
+# PREPARATIONS
+MINE_GRID = []
+NUMBER_GRID = []
+USER_GRID = []
+REVEALED = set()
+UNOPENED_FIELDS = GRID_SIZE*GRID_SIZE  # not a constant
 
 def populate_user_grid(grid):
     global GRID_SIZE
@@ -160,53 +173,54 @@ def place_numbers(mine_grid, grid):
                 sorounding_mines = 0
 
 def reveal_neighbours(x, y):
-    global GRID_SIZE, number_grid
+    global GRID_SIZE, NUMBER_GRID, REVEALED
 
     if ( x >= 0 and x <= GRID_SIZE-1) and (y >= 0 and y <= GRID_SIZE-1):
-        if number_grid[x][y]:  # and n_grid[x][y] != -1:
-            return (x,y)
+        if (x,y) in REVEALED:
+            # print("Revealed pair found {} {}".format(x,y))
+            pass
         else:
-            reveal_neighbours(x+1, y)
-            reveal_neighbours(x-1, y)
-            reveal_neighbours(x, y+1)
-            reveal_neighbours(x, y-1)
-            return (x,y)
-    else:
-        pass
-    
+            if NUMBER_GRID[x][y]:  # and n_grid[x][y] != -1:
+                REVEALED.add((x, y))
+            else:
+                REVEALED.add((x, y))
+                reveal_neighbours(x+1, y)
+                reveal_neighbours(x-1, y)
+                reveal_neighbours(x, y+1)
+                reveal_neighbours(x, y-1)
+
 def check_for_mines(x, y, m_grid, n_grid, u_grid):
-    global ALIVE
-    queue = []
+    global ALIVE, REVEALED, GREEN, RESET, RED
     if m_grid[x][y]:
         print("You died.")
         ALIVE = False
+        for i in range(GRID_SIZE):
+            for j in range(GRID_SIZE):
+                if m_grid[i][j]:
+                    u_grid[i][j] = RED + "M" + RESET
+        print("\n".join(" ".join(i for i in item) for item in USER_GRID))
     else:
-        queue = reveal_neighbours(x, y)
-        for q in queue:
-            if q:
-                x, y = q
-                u_grid[x][y] = str(n_grid[x][y])
+        reveal_neighbours(x, y)
+        for coord in REVEALED:
+            x, y = coord
+            u_grid[x][y] = GREEN + str(n_grid[x][y]) + RESET if n_grid[x][y] > 0 else BLUE + str(n_grid[x][y]) + RESET
 
-# PREPARATIONS            
-mine_grid = []
-number_grid = []
-user_grid = []
-
-populate_user_grid(user_grid)
-place_mines(mine_grid)
-pre_populate_number_grid(number_grid)
-place_numbers(mine_grid, number_grid)
+populate_user_grid(USER_GRID)
+place_mines(MINE_GRID)
+pre_populate_number_grid(NUMBER_GRID)
+place_numbers(MINE_GRID, NUMBER_GRID)
 
 for i in range(GRID_SIZE):
-    print(mine_grid[i])
+    print(MINE_GRID[i])
 print()
 for i in range(GRID_SIZE):
-    print(number_grid[i])
+    print(NUMBER_GRID[i])
     
 # THE GAME LOOP
-while ALIVE:
+while ALIVE and MINES != UNOPENED_FIELDS:
     print()
-    print("\n".join(" ".join(i for i in item) for item in user_grid))
-    print("Enter two numbers between 0 and 19: ")
+    print("\n".join(" ".join(i for i in item) for item in USER_GRID))
+    print("Enter two numbers between 0 and {} (fields remaining {}): ".format(GRID_SIZE-1, UNOPENED_FIELDS))
     x,y = map(int, input().split())
-    check_for_mines(x, y, mine_grid, number_grid, user_grid)
+    check_for_mines(x, y, MINE_GRID, NUMBER_GRID, USER_GRID)
+    UNOPENED_FIELDS = len([i for u in USER_GRID for i in u if i == "?"])
